02beb5a31f899c15c49668719d99f388036749a1
internHannah <hannah.nguyen@tarmac.io>
2024-06-11 10:35:07 +0700

    Commit on INPR-3: create the main page

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+import React from "react";
+
+
+function App() {
+    
+    return (
+            <div className="App">
+            <div className="App__credit">Credits go to Hannah</div>
+            <div className="App__header">
+                <h1> Totally Legit Mars Weather App for Time Travelers</h1>
+                </div>
+            </div>
+    );
+}
+
+export default App;
