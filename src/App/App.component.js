167d2e9a426f05cfdb60b68d7e3d194f8d13f57e
internHannah <hannah.nguyen@tarmac.io>
2024-06-14 17:47:16 +0700

    INPR-3: run prettier on all files and change &__credit to Header__Credit

--- src/App/App.component.js
+++ src/App/App.component.js
@@ -1,13 +1,13 @@
-import './App.scss';
-import Header from '../Header/Header.component';
+import "./App.scss";
+import Header from "../Header/Header.component";
 import React from "react";
 
-function App() { 
+function App() {
   return (
     <div className="App">
       <Header />
     </div>
-    );
+  );
 }
 
 export default App;
--- src/Header/Header.component.js
+++ src/Header/Header.component.js
@@ -1,11 +1,11 @@
-import './Header.scss';
+import "./Header.scss";
 import React from "react";
 
-function Header() { 
+function Header() {
   return (
     <div className="Header">
-      <div className="Header__credit">Credits go to Hannah</div>
-        <h1> Totally Legit Mars Weather App for Time Travelers</h1>
+      <div className="Header__Credit">Credits go to Hannah</div>
+      <h1> Totally Legit Mars Weather App for Time Travelers</h1>
     </div>
   );
 }
