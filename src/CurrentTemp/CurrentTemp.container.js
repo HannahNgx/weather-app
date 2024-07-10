00a06d4f2d0dcd172757c5841ea7a311f2d35752
internHannah <hannah.nguyen@tarmac.io>
2024-07-10 20:26:47 +0700

    INPR-5: display no data if not available

--- src/CurrentTemp/CurrentTemp.container.js
+++ src/CurrentTemp/CurrentTemp.container.js
@@ -3,21 +3,30 @@ import CurrentTempComponent from './CurrentTemp.component';
 
 function CurrentTemp() {
   const [currentTemp, setCurrentTemp] = useState({ lowest: null, highest: null });
+  const [error, setError] = useState(null);
 
   const fetchCurrentTemp = async () => {
-    const response = await fetch('http://localhost:4567/insight_weather');
-    const data = await response.json();
-    const sol_keys = Object.keys(data);
-    setCurrentTemp({
-      highest: data[sol_keys[0]].PRE.mx.toFixed(2),
-      lowest: data[sol_keys[0]].PRE.mn.toFixed(2),
-    });
+    try {
+      const response = await fetch('http://localhost:4567/insight_weather');
+      const data = await response.json();
+      const sol_keys = Object.keys(data);
+      const { PRE } = data[sol_keys[0]];
+      setCurrentTemp({
+        highest: PRE.mx.toFixed(2),
+        lowest: PRE.mn.toFixed(2),
+      });
+    } catch (error) {
+      setError(true);
+    }
   }
 
   useEffect(() => {
     fetchCurrentTemp();
   }, []);
 
+  if (error) {
+    return <CurrentTempComponent currentTemp={{ lowest: '(no data)', highest: '(no data)' }} />;
+  }
   return <CurrentTempComponent currentTemp={currentTemp} />;
 };
 
