cc1baab35def27479d102354a93672220710a95f
internHannah <hannah.nguyen@tarmac.io>
2024-08-06 00:07:49 +0700

    INPR-9: use props in unit.component.js

--- src/Unit/Unit.component.js
+++ src/Unit/Unit.component.js
@@ -1,40 +1,38 @@
 import './Unit.scss';
 import React, { useMemo } from 'react';
-import { getFixedValue } from '../helpers';
-import { celsiusToFahrenheit } from '../helpers';
+import { getFixedValue, celsiusToFahrenheit } from '../helpers';
 
-export function UnitComponent({label, value, isCelsius, toggleUnit}) {
+export function UnitComponent({ label, value, isCelsius, toggleUnit }) {
   const fValue = getFixedValue(celsiusToFahrenheit(value));
-  const { TempComponentWrapper } = useMemo(() => {
+  const { TempComponentWrapper, tempComponentProps } = useMemo(() => {
     const componentWrapper = toggleUnit ? 'button' : 'span';
     let componentProps = {};
-    
     if (toggleUnit) {
       componentProps.onClick = toggleUnit;
     }
-    
     return {
       TempComponentWrapper: componentWrapper,
       tempComponentProps: componentProps,
     };
   }, [toggleUnit]);
+
   return (
     <div className="UnitComponent">
-    <b>{label}</b>
-    {isCelsius ? value : fValue}
-    <TempComponentWrapper
-      className={`UnitComponent__Unit ${isCelsius ? 'UnitComponent__Unit--selected' : ''}`}
-      onClick={toggleUnit}
-    >
-      ºC
-    </TempComponentWrapper>
-    /
-    <TempComponentWrapper
-      className={`UnitComponent__Unit ${!isCelsius ? 'UnitComponent__Unit--selected' : ''}`}
-      onClick={toggleUnit}
-    >
-      ºF
-    </TempComponentWrapper>
-  </div>
-  )
+      <b>{label}</b>
+      {isCelsius ? value : fValue}
+      <TempComponentWrapper
+        className={`UnitComponent__Unit ${isCelsius ? 'UnitComponent__Unit--selected' : ''}`}
+        {...tempComponentProps}
+      >
+        ºC
+      </TempComponentWrapper>
+      /
+      <TempComponentWrapper
+        className={`UnitComponent__Unit ${!isCelsius ? 'UnitComponent__Unit--selected' : ''}`}
+        {...tempComponentProps}
+      >
+        ºF
+      </TempComponentWrapper>
+    </div>
+  );
 }
